3ad959ac48b9fb120b19b10f32bb84245a02cc5b
0xAdrain <0xAdrian.Wayne@proton.me>
2026-03-04 00:11:03 +0800

    Point monitor iframe to inner site

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD (override via query params)
-        iframe.src = 'https://os.henryheffernan.com/';
+        iframe.src = 'https://portfolio-inner-site-tan.vercel.app/';
         /**
          * Use dev server is query params are present
          *
